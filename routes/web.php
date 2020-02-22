1cc875bb6f6109c97bef7bcac461609d1cc134e7
carlosguttemberg <guttembergcarlos@gmail.com>
2020-02-22 17:58:34 -0300

    Criado controller de login do administrador

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/admin', 'AdminController@index')->name('homeAdmin');
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
 
+Route::get('/admin/login', 'Auth\AdminLoginController@index')->name('admin.login');
+
+Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
